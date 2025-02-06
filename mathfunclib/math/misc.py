0c7cf70de62769d5757f93323c2bc7d9273b77a9
CrSb0001 <127436709+CrSb0001@users.noreply.github.com>
2025-02-06 13:22:49 -0600

    Update misc.py

--- mathfunclib/math/misc.py
+++ mathfunclib/math/misc.py
@@ -1,6 +1,7 @@
 # Some miscellaneous functions that can come in handy for Project Euler problems.
 
-from math import cos,sin,sqrt,exp,ceil,floor,pi,exp
+import typing
+from math import cos,sin,sqrt,exp,ceil,floor,pi,gcd
 from decimal import *
 
 def compute_sin(N):
@@ -39,3 +40,24 @@ def compute_cos(N):
             if t2<m:
                 m,i=t2,n
     return i
+
+def sum_multiples_under_lim(m,n,lim):
+    if type(m)!=int or type(n)!=int:
+        return "All parameters must be integers."
+    if lim!=float('inf') and type(lim)!=int:
+        return "Parameter [lim] must be integer if not float('inf')"
+    if lim<=0:
+        return "Parameter [lim] must be greater than 0."
+    
+    def res(u,v,d=0):
+        if d!=float('inf'):
+            q=(v-1)//u
+            return u*q*(q-1)//2
+        else: # THIS part is helpful for the bonus Project Euler -1.
+            q=(u+v-u*v)//gcd(u+v-u*v,12)
+            r=12//gcd(u+v-u*v,12)
+            if r==1:
+                return f'{q}'
+            else:
+                return f'{q}/{r}'
+    return res(m,lim)+res(n,lim)-res(n*m,lim) if lim!=float('inf') else res(n,m,lim)
